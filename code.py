c74c08ba6f71a96cd654d522fd5a934a8323a00a
lpalazzi <lpalazzi@outlook.com>
2020-08-18 19:11:45 -0400

    init commit

---
+++ code.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+def blight_model():
+    
+    # Load data from the csv files
+
+    # Data preprocessing
+    # --filter out Null values in compliance column
+    # --separate into X_train, y_train, and X_test
+    # --remove unnecessary features to prevent data leakage
+    
+    # Select classifier models to test
+    # --make a custom class that will keep track of all the models and their hyperparameters, and store the evaluation results throughout the process
+
+    # Use cross validation with all of the models
+
+    # Re-train the best model using full training set and evaluate
+    
+    return # Your answer here
+
+blight_model()
