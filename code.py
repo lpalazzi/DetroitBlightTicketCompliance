723a60d346986c4b04c90fc512295c7549161dc8
lpalazzi <lpalazzi@outlook.com>
2020-09-30 21:37:20 -0400

    final submission, grade 100%

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import datetime as dt
+from sklearn.ensemble import GradientBoostingClassifier
 
 def process_data(df, addresses):
     # ------------------------------
@@ -9,28 +9,21 @@ def process_data(df, addresses):
 
     # Replace address columns with latitude and longitude 
     df = pd.merge(df, addresses, on='ticket_id', how='left').drop(['violation_street_number', 'violation_street_name', 'violation_zip_code'], axis=1)
-    
-    # Replace other spellings of Detroit with 'DETROIT'
-    detroit_spellings = {'Deroit', 'DEROT', 'DERROIT', 'DERTOIT', 'DERTROIT', 'DEYROIT', 'DFETROIT', 'DKETROIT', 'DRTROIT', 'DTEROIT', 'DTROIT', 'DWETROIT', 'Det', 'DET ,', 'DET ROIT', 'DET,', 'DET,.', 'Det.', 'DET. MI.', 'DET.,', 'DET., MI.', 'det48234', 'DETAROIT', 'DETEOIT', 'DETEROIT', 'DETOIT', 'DETORIIT', 'DETORIT', 'DETOUR', 'DETOUR VILLAGE', 'DETR4OIT', 'DETREOIT', 'DETRIOIT', 'detriot', 'DETRIT', 'DETRIUT', 'DETRJOIT', 'Detro;it', 'Detrofit', 'Detroi', 'DETROIIT', 'DETROIOT', 'DETROIR', 'DETROIRT', 'DETROIS', 'Detroit', 'DETROIT  4', 'DETROIT,', 'DETROIT, MI.', 'DETROIT, MI. 48206', 'detroit,mi', 'DETROIT`', 'DETROIT1', 'DETROITdetroit', 'Detroitf', 'DETROITI', 'DETROITL', 'Detroitli', 'DETROITM', 'DETROITQ', 'detroitt', 'DETROITY', 'DETROIYT', 'DETROKT', 'DETROOIT', 'DETROPIT', 'detrorit', 'DETROT', 'DETROTI', 'DETROUIT', 'DETRPOT', 'DETRROIT', 'DETRTOI', 'detrtoit', 'DETTROIT', 'DETTROITM', 'DETYROIT'}
-    df['city'] = df['city'].replace(to_replace=detroit_spellings, value='DETROIT')
 
-    # Convert dates to DateTime
-    df['ticket_issued_date'] = pd.to_datetime(df['ticket_issued_date'], errors='coerce', yearfirst=True)
-    df['hearing_date'] = pd.to_datetime(df['hearing_date'], errors='coerce', yearfirst=True)
-
-    # New feature: days between ticket issue and hearing date
-    df['date_delta'] = (df['hearing_date'].fillna(dt.datetime(year=2200,month=1,day=1)) - df['ticket_issued_date']) / np.timedelta64(1, 'D')
+    # Remove where lat or lon is null
+    df['lat'] = df['lat'].fillna(0)
+    df['lon'] = df['lon'].fillna(0)
 
     # New feature: mailing address not in USA (information in a few different features)
     df['non_usa'] = ( df['state'].isna() ) | ( df['non_us_str_code'].notna() ) | ( df['country'].apply(lambda x: False if ('USA' in str(x)) else True) )
 
     # New feature: mailing address not in Michigan
     df['non_michigan'] = df['state'].apply(lambda x: False if ('MI' in str(x)) else True)
-    
-    # Drop some unnecessary columns
-    df = df.drop(['mailing_address_str_number', 'mailing_address_str_name', 'zip_code', 'non_us_str_code', 'country', 'state', 'ticket_issued_date', 'hearing_date'], axis=1)
-    
-    return df
+
+    # Pick the columns to include in the final DataFrame
+    df_final = df[['ticket_id', 'judgment_amount', 'lat', 'lon', 'non_usa', 'non_michigan', 'compliance']]
+
+    return df_final
 
 
 def blight_model():
@@ -43,22 +36,34 @@ def blight_model():
     # Drop the training set columns not present in test set
     train = train.drop(['payment_amount', 'payment_date', 'payment_status', 'balance_due', 'collection_status', 'compliance_detail'], axis=1)
 
+    # Drop the training set rows where "not responsible" (tickets where the violators were found not responsible are not considered during evaluation)
+    train = train.dropna(subset=['compliance'])
+
+    # Add an empty column to test set for 'compliance'
+    test['compliance'] = np.nan
+
+    # Convert compliance floats to integer
+    train['compliance'] = train['compliance'].astype(int)
+
     # Pre-preocess the train and test sets
     train = process_data(train, addresses)
     test  = process_data(test,  addresses)
 
+    # Split into X, y
+    X_train = train[['judgment_amount', 'lat', 'lon', 'non_usa', 'non_michigan']]
+    X_test  = test [['ticket_id', 'judgment_amount', 'lat', 'lon', 'non_usa', 'non_michigan']]
+    y_train = train['compliance']
+    y_test  = pd.Series(data=np.nan, index=test['ticket_id'])
+
     # Free up dataframes no longer used
     del addresses
 
-    # Select classifier models to test
-    # --make a custom class that will keep track of all the models and their hyperparameters, and store the evaluation results throughout the process
+    # Create and train model
+    model = GradientBoostingClassifier(random_state=0)
+    model.fit(X_train, y_train)
 
-    # Use cross validation with all of the models
+    for ticket_id, probability in y_test.iteritems():
+        test_sample = X_test[X_test['ticket_id'] == ticket_id].drop(['ticket_id'], axis=1)
+        y_test.loc[ticket_id] = model.predict_proba(test_sample)[:,1]
 
-    # Re-train the best model using full training set and evaluate
-
-    print (train['date_delta'].head(15))
-
-    return # Your answer here
-
-blight_model()
+    return y_test
